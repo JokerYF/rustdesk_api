6af876f430479de0f6e14780cb97d8f3f928a418
御风 <eternallyyf@163.com>
2025-11-10 12:46:14 +0800

    refactor(auth): 移除视图函数的@login_required装饰器 - 删除了common.py中视图函数的@login_required装饰器 - 保留了手动检查Authorization头部的逻辑 - 确保认证逻辑的一致性处理

--- apps/client_apis/common.py
+++ apps/client_apis/common.py
@@ -4,7 +4,6 @@ import time
 import traceback
 from functools import wraps
 
-from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, JsonResponse, HttpResponse
 
 from apps.db.service import TokenService, PeerInfoService
@@ -22,7 +21,6 @@ def check_login(func):
     """
 
     @wraps(func)
-    @login_required
     def wrapper(request: HttpRequest, *args, **kwargs):
         headers = request.headers
         if 'Authorization' not in headers:
